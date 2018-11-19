dc30c175cc19df4c1c84ff22224413ac214fbb55
Nicolas Wagner <contact@wagner-nicolas.com>
2018-11-19 17:11:33 +0200

    set up router

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -2,26 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { Provider } from 'react-redux'
+import { Router } from "@reach/router"
 
 import Initializer from './initializer'
 import Balance from '../containers/balance'
 
 import './app.css'
 
+const NotFound = () => (
+  <div>Sorry, nothing here.</div>
+)
+
 const App = ({ store, testElement }) => (
   <Provider store={store}>
     <Initializer>
-        <div id="router-root">
-          <Helmet>
-            <title>Kleros Dapp</title>
-          </Helmet>
-          <div className="App">
-            <header className="App-header">
-            <Balance />
-            </header>
-          </div>
-          {testElement}
+      <React.Fragment>
+        <Helmet>
+          <title>Kleros Dapp</title>
+        </Helmet>
+        <div className="App">
+          <Router>
+            <Balance path="/" />
+            <NotFound default />
+          </Router>
         </div>
+        {testElement}
+      </React.Fragment>
     </Initializer>
   </Provider>
 )
