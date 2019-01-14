856c3468e0104db2fa6ee167910a9fd1e54f8df6
Nicolas Wagner <contact@wagner-nicolas.com>
2019-01-14 11:30:05 +0100

    feat(notifications): add subscribe notification view

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -9,6 +9,7 @@ import Home from '../containers/home'
 import ArbitrableTx from '../containers/arbitrable-tx'
 import New from '../containers/arbitrable-tx/new'
 import Resume from '../containers/arbitrable-tx/resume'
+import Notifications from '../components/notifications'
 import { ReactComponent as Kleros } from '../assets/kleros.svg'
 import { ReactComponent as Plus } from '../assets/plus.svg'
 
@@ -26,7 +27,8 @@ const Main = ({ children }) => (
           <li><a href='https://kleros.io'><Kleros style={{height: '35px', position: 'relative', top: '13px'}} /></a></li>
           <li onClick={() => navigate('/')}>Escrow</li>
           <li onClick={() => navigate('/new')} style={{float: 'right'}}><span className='btn-new'><Plus style={{width: '19px', height: '35px', paddingRight: '8px', position: 'relative', top: '12px'}} />New Transaction</span></li>
-          <li onClick={() => navigate('/')} style={{float: 'right', color: '#a892c5'}}>My Transactions</li>
+          <li onClick={() => navigate('/')} style={{float: 'right', color: '#a892c5', paddingRight: '50px'}}>My Transactions</li>
+          <li onClick={() => navigate('/notifications')} style={{float: 'right', color: '#a892c5', paddingRight: '50px'}}>Notifications</li>
         </ul>
       </nav>
     </header>
@@ -49,6 +51,7 @@ const App = ({store}) => (
             <New path="new" />
             <Resume path="resume/:metaEvidenceIPFSHash" />
             <ArbitrableTx path="/:arbitrableTxId" />
+            <Notifications path="notifications" />
             <NotFound default />
           </Main>
         </Router>
---
+++ src/components/notifications/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+
+import Button from '../button'
+import { ReactComponent as Email } from '../../assets/plus-purple.svg'
+
+import './notifications.css'
+
+const Notifications = () => (
+  <div className='NewArbitrableTx'>
+    <h1 className='NewArbitrableTx-h1'><Email style={{width: '20px', height: '35px', position: 'relative', top: '11px', paddingRight: '8px'}} />Notifications</h1>
+    <Formik
+      initialValues={{ email: '' }}
+      validate = {values => {
+        {/* TODO use Yup */}
+        let errors = {}
+        if (!values.email)
+          errors.email = 'Email Required'
+        if (
+          values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={email => {}}
+    >
+      {({ setFieldValue }) => (
+        <Form className='FormNewArbitrableTx'>
+          <div className='section-title'>
+            <label htmlFor='email'>Email</label>
+            <Field name='email' placeholder='email' />
+            <ErrorMessage name='email' component='div' className='error'/>
+          </div>
+          <div className='section-submit'>
+            <Button type='submit'>Enable Notifications</Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)
+
+export default Notifications
