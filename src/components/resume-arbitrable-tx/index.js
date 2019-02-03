b049f05c617cf0443ee32145be2032cf53b34b98
Nicolas Wagner <contact@wagner-nicolas.com>
2019-02-04 00:27:47 +0200

    feat(resume): add message `copied`

--- src/components/resume-arbitrable-tx/index.js
+++ src/components/resume-arbitrable-tx/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as arbitrabletxSelectors from '../../reducers/arbitrable-transaction'
 import { ReactComponent as PrimaryDocument } from '../../assets/primary-document.svg'
@@ -7,82 +7,93 @@ import { ReactComponent as Copy } from '../../assets/copy.svg'
 
 import './resume-arbitrable-tx.css'
 
-const ResumeArbitrableTx = ({ arbitrabletx, title, children, footer }) => (
-  <React.Fragment>
-    <div className='ResumeArbitrableTx'>
-      <h1 className='ResumeArbitrableTx-h1'>{title}</h1>
-      <div className='ResumeArbitrableTx-ContentNewArbitrableTx'>
-        <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Title</div>
-        <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.title}</div>
+const ResumeArbitrableTx = ({ arbitrabletx, title, children, footer }) => {
+  const [copied, setCopied] = useState(false)
+  return (
+    <>
+      <div className='ResumeArbitrableTx'>
+        <h1 className='ResumeArbitrableTx-h1'>{title}</h1>
+        <div className='ResumeArbitrableTx-ContentNewArbitrableTx'>
+          <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Title</div>
+          <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.title}</div>
 
-        {arbitrabletx.description && (
-          <React.Fragment>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Description</div>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.description}</div>
-          </React.Fragment>
-        )}
+          {arbitrabletx.description && (
+            <>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Description</div>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.description}</div>
+            </>
+          )}
 
-        <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Sender</div>
-        <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.seller}</div>
-        
-        {arbitrabletx.amount > 0 && (
-          <React.Fragment>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Amount</div>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.amount} ETH</div>
-          </React.Fragment>
-        )}
+          <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Sender</div>
+          <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.seller}</div>
+          
+          {arbitrabletx.amount > 0 && (
+            <>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Amount</div>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>{arbitrabletx.amount} ETH</div>
+            </>
+          )}
 
-        {arbitrabletx.file && (
-          <React.Fragment>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Primary Document</div>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content' style={{border: '0'}}>
-              <a href={arbitrabletx.file} alt='Primary Document' target='_blank' rel='noopener noreferrer'>
-                <PrimaryDocument />
-              </a>
-            </div>
-          </React.Fragment>
-        )}
+          {arbitrabletx.file && (
+            <>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Primary Document</div>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content' style={{border: '0'}}>
+                <a href={arbitrabletx.file} alt='Primary Document' target='_blank' rel='noopener noreferrer'>
+                  <PrimaryDocument />
+                </a>
+              </div>
+            </>
+          )}
 
-        {arbitrabletx.shareLink && (
-          <React.Fragment>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Share Transaction</div>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>
-              {arbitrabletx.shareLink}
-              {
-              /* Logical shortcut for only displaying the
-                  button if the copy command exists */
-              document.queryCommandSupported('copy') &&
-                <Copy style={{marginLeft: '1em', cursor: 'pointer'}} onClick={() => {navigator.clipboard.writeText(arbitrabletx.shareLink)}} />
-              }
-            </div>
-          </React.Fragment>
-        )}
+          {arbitrabletx.shareLink && (
+            <>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Share Transaction</div>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content'>
+                {arbitrabletx.shareLink}
+                {
+                /* Logical shortcut for only displaying the
+                    button if the copy command exists */
+                  document.queryCommandSupported('copy') && (
+                    !copied ? (
+                      <Copy 
+                        style={{marginLeft: '1em', cursor: 'pointer'}} 
+                        onClick={() => {navigator.clipboard.writeText(arbitrabletx.shareLink) && setCopied(true)}} 
+                      /> 
+                    ) : (
+                      <span style={{marginLeft: '1em', color: '#4004a3'}}>Copied</span>
+                    )
+                  ) 
+                }
+              </div>
+            </>
+          )}
 
-        {arbitrabletx.evidences && (
-          <React.Fragment>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Evidences</div>
-            <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content' style={{border: '0'}}>
-              {
-                arbitrabletx.evidences.map((evidence, index) => (
-                  <Attachment
-                    URI={evidence.evidenceJSON.fileURI}
-                    title={evidence.evidenceJSON.name}
-                    description={evidence.evidenceJSON.description}
-                    key={index}
-                  />
-                ))
-              }
-            </div>
-          </React.Fragment>
-        )}
+          {arbitrabletx.evidences && (
+            <>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-name'>Evidences</div>
+              <div className='ResumeArbitrableTx-ContentNewArbitrableTx-content' style={{border: '0'}}>
+                {
+                  arbitrabletx.evidences.map((evidence, index) => (
+                    <Attachment
+                      URI={evidence.evidenceJSON.fileURI}
+                      title={evidence.evidenceJSON.name}
+                      description={evidence.evidenceJSON.description}
+                      key={index}
+                    />
+                  ))
+                }
+              </div>
+            </>
+          )}
+        </div>
+        <div className= 'ResumeArbitrableTx-section-submit'>
+          {children}
+        </div>
       </div>
-      <div className= 'ResumeArbitrableTx-section-submit'>
-        {children}
-      </div>
-    </div>
-    {footer}
-  </React.Fragment>
-)
+      {footer}
+    </>
+  )
+}
 
 ResumeArbitrableTx.propTypes = {
   // State
