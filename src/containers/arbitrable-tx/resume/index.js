0b7d3d68d67bf85e167c8ccc1890b66ae7fcd677
Sam Vitello <sam.j.vitello@gmail.com>
2020-02-05 16:01:17 -0700

    feat: show two tx notice on token txs

--- src/containers/arbitrable-tx/resume/index.js
+++ src/containers/arbitrable-tx/resume/index.js
@@ -7,6 +7,7 @@ import { ClipLoader } from 'react-spinners'
 import * as arbitrabletxActions from '../../../actions/arbitrable-transaction'
 import * as arbitrabletxSelectors from '../../../reducers/arbitrable-transaction'
 import ResumeArbitrableTx from '../../../components/resume-arbitrable-tx'
+import Notice from '../../../components/notice'
 import Button from '../../../components/button'
 import CopyImg from '../../../assets/copy.png'
 
@@ -42,59 +43,65 @@ class Resume extends PureComponent {
             <ResumeArbitrableTx
               arbitrabletx={arbitrabletxForm}
               title={'Invoice Details'}
-            />
-            <div className="additional-options">
-              <div className="buttons">
-                <div className="link-share" style={{ textAlign: 'left' }}>
-                  {/*
-                        Logical shortcut for only displaying the
-                        button if the copy command exists
-                      */
-                  document.queryCommandSupported('copy') &&
-                    <Button id="copy-button" onClick={
-                        () => {
-                          navigator.clipboard.writeText(shareLink)
-                          document.getElementById("copy-button").style.background = "#007acc"
-                          setTimeout(() => {
-                            document.getElementById("copy-button").style.background = "#009aff"
-                          }, 100)
-                        }
-                      }>
-                      <img src={CopyImg} alt="copy-link-icon" />
-                      Copy link to share
-                    </Button>
-                  }
-                </div>
-                <Formik
-                  onSubmit={() =>
-                    createArbitrabletx(arbitrabletxForm, metaEvidenceIPFSHash)
-                  }
-                >
-                  {({ isSubmitting }) => (
-                    <Form className={'PayOrReimburseArbitrableTx'}>
-                      <Button
-                        type="submit"
-                        disabled={isSubmitting}
-                      >
-                        {isSubmitting && (
-                          <span
-                            style={{
-                              position: 'relative',
-                              top: '4px',
-                              lineHeight: '40px',
-                              paddingRight: '4px'
-                            }}
-                          >
-                            <ClipLoader size={20} color={'#fff'} />
-                          </span>
-                        )}{' '}
-                        Submit Payment
+            >
+              <div className="additional-options">
+                {
+                  arbitrabletxForm.token && arbitrabletxForm.token.address ? (
+                    <Notice text={<span>TWO TRANSACTIONS REQUIRED<br /><br />This action requires two transactions to be signed at the same time from MetaMask<br /><br />MetaMask may warn you that the second transaction will fail. Please ignore this warning.</span>} />
+                  ) : ''
+                }
+                <div className="buttons">
+                  <div className="link-share" style={{ textAlign: 'left' }}>
+                    {/*
+                          Logical shortcut for only displaying the
+                          button if the copy command exists
+                        */
+                    document.queryCommandSupported('copy') &&
+                      <Button id="copy-button" onClick={
+                          () => {
+                            navigator.clipboard.writeText(shareLink)
+                            document.getElementById("copy-button").style.background = "#007acc"
+                            setTimeout(() => {
+                              document.getElementById("copy-button").style.background = "#009aff"
+                            }, 100)
+                          }
+                        }>
+                        <img src={CopyImg} alt="copy-link-icon" />
+                        Copy link to share
                       </Button>
-                    </Form>
-                  )}
-                </Formik>
+                    }
+                  </div>
+                  <Formik
+                    onSubmit={() =>
+                      createArbitrabletx(arbitrabletxForm, metaEvidenceIPFSHash)
+                    }
+                  >
+                    {({ isSubmitting }) => (
+                      <Form className={'PayOrReimburseArbitrableTx'}>
+                        <Button
+                          type="submit"
+                          disabled={isSubmitting}
+                        >
+                          {isSubmitting && (
+                            <span
+                              style={{
+                                position: 'relative',
+                                top: '4px',
+                                lineHeight: '40px',
+                                paddingRight: '4px'
+                              }}
+                            >
+                              <ClipLoader size={20} color={'#fff'} />
+                            </span>
+                          )}{' '}
+                          Submit Payment
+                        </Button>
+                      </Form>
+                    )}
+                  </Formik>
+                </div>
               </div>
-            </div>
+            </ResumeArbitrableTx>
           </div>
         )}
         {arbitrabletxForm.amount !== undefined && !arbitrabletxForm.invoice && (
@@ -102,6 +109,11 @@ class Resume extends PureComponent {
             arbitrabletx={arbitrabletxForm}
             title={'Payment Details'}
           >
+            {
+              arbitrabletxForm.token && arbitrabletxForm.token.address ? (
+                <Notice text={<span>TWO TRANSACTIONS REQUIRED<br /><br />This action requires two transactions to be signed at the same time from MetaMask<br /><br />MetaMask may warn you that the second transaction will fail. Please ignore this warning.</span>} />
+              ) : ''
+            }
             <Formik
               onSubmit={() =>
                 createArbitrabletx(arbitrabletxForm, metaEvidenceIPFSHash)
